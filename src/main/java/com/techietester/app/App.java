0f6dba99324f3e6f429826495a366f751813f66c
James Willett <james.willett@sap.com>
2019-10-10 20:19:50 +0100

    Updating to Gradle 5, and other dependency updates

--- src/main/java/com/techietester/app/App.java
+++ src/main/java/com/techietester/app/App.java
@@ -2,7 +2,7 @@ package com.techietester.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
